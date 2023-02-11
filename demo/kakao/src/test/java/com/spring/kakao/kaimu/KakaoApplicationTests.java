095ec9b7cc44faab3e99988e54ebf8b218fe2c70
AkimJemi <rlawoals623@gmail.com>
2023-02-11 15:11:14 +0900

    kakao_project

---
+++ demo/kakao/src/test/java/com/spring/kakao/kaimu/KakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.kakao.kaimu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
